c935c68103eea916bb65912eb6119b58f8262960
Wilker Atalaya R <antru.wilker@gmail.com>
2023-06-12 15:26:24 -0500

    cambiar task

--- Formacion FrontEnd/DOM/script.js
+++ Formacion FrontEnd/DOM/script.js
@@ -1,9 +1,23 @@
 const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
-    evento.preventDefault();
+    evento.preventDefault(); // para no refrescar la pantalla
     const input = document.querySelector("[data-form-input]");
+    const value = input.value;
     console.log(input.value);
+    input.value = ''; // dejar el input vacio una vez realizada la accion
+
+    const task = document.querySelector('[data-task]');
+
+    const content = `<div>
+        <i class="fas fa-check-square icon"></i>
+        <span class="task"> ${value}</span>
+        </div>
+        <i class="fas fa-trask-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+
+
+    console.log(content);
 }
 
 console.log(btn);

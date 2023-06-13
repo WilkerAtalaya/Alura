d83e305838fffaafbc9b6e50456ac38b06cf9b93
Wilker Atalaya R <antru.wilker@gmail.com>
2023-06-13 01:27:45 -0500

    class03 complete

--- Formacion FrontEnd/DOM/script.js
+++ Formacion FrontEnd/DOM/script.js
@@ -1,31 +1,50 @@
-const btn = document.querySelector('[data-form-btn]');
+( () => {
+    const btn = document.querySelector('[data-form-btn]');
 
-const createTask = (evento) => {
-    evento.preventDefault(); // para no refrescar la pantalla
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    console.log(input.value);
+    const createTask = (evento) => {
+        evento.preventDefault(); // para no refrescar la pantalla
+        const input = document.querySelector("[data-form-input]");
+        const value = input.value;
 
-    const list = document.querySelector("[data-list]")
-    input.value = ''; // dejar el input vacio una vez realizada la accion
+        const list = document.querySelector("[data-list]")
+        input.value = ''; // dejar el input vacio una vez realizada la accion
 
-    const task = document.createElement("li");
-    task.classList.add("card");
+        const task = document.createElement("li");
+        task.classList.add("card");
 
-    const content = `<div>
-        <i class="fas fa-check-square icon"></i>
-        <span class="task"> ${value}</span>
-        </div>
-        <i class="fas fa-trask-alt trashIcon icon"></i>`
-    task.innerHTML = content;
+        const taskContent = document.createElement('div');
+    
 
-    list.appendChild(task);
-    console.log(content);
-}
+        const titleTask = document.createElement('span');
+        titleTask.classList.add('task');
+        titleTask.innerText = value;
+        taskContent.appendChild(checkComplete());
+        
+        taskContent.appendChild(titleTask);
 
-console.log(btn);
+        const content = `
+            <i class="fas fa-trask-alt trashIcon icon"></i>`
+        //task.innerHTML = content;
+        task.appendChild(taskContent);
 
-// Arrow functions o funciones anonimas
-btn.addEventListener("click", createTask);
+        list.appendChild(task);
+    };
 
+    // Arrow functions o funciones anonimas
+    btn.addEventListener("click", createTask);
 
+    const checkComplete = () => {
+        const i = document.createElement("i");
+        i.classList.add("far","fa-check-square","icon");
+        i.addEventListener("click", completeTask);
+        return i;
+    }
+
+    const completeTask = (event) => {
+        const element = event.target;
+        element.classList.toggle("fas");  //añadir class
+        element.classList.toggle("far"); // quitar class
+        element.classList.toggle("done");
+    }
+
+}) ();
